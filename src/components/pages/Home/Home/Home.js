89e35189fc7c67af9cc23b902afc0a4f7bc06c31
ruhulaminbd93 <ruhulaminbd93@gmail.com>
2022-01-26 15:40:01 +0600

    footer

--- src/components/pages/Home/Home/Home.js
+++ src/components/pages/Home/Home/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import Footer from "../../../shared_components/Footer/Footer";
 import Slider from "../Slider/Slider";
 
 const Home = () => {
-  return <Slider />;
+  return (
+    <div>
+      <Slider />
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
